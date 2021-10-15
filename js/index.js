1345063709cee706b16ed7103feaba286e8066c9
Gyeojin <gohyeojin@gmail.com>
2021-10-15 19:01:08 +0900

    211015_도트 작업 완료+슬라이드 작업 애니메이션 진행중

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-const conTxt = document.querySelectorAll('.slide .txt p');
+// $(function () {
+//   const selector = $(".slick-dots");
+//   console.log(selector);
+//   // for (let i = 0; i < nameArr.length; i++) {
+//   //   $(".slick-dots li").data("thumb", nameArr[i]);
+//   //   console.log(nameArr[i]);
+//   // }
+// });
 
-conTxt.forEach(element => {
-  const cutTxt = element.textContent.slice(0, 70) + "...";
-  element.textContent = cutTxt;
-});
+const textBox = document.querySelectorAll(".text_box p.desc");
+const textWidth = textBox.offsetWidth; //500px
+// const desc = textBox.lastChild.length;
+for (i = 0; i < textBox.length; i++) {
+  var desc = textBox[i].lastChild.length;
+  console.log(desc[i].offsetWidth);
+  // for (j = 0; j < desc.length; j++){
+  //   if(desc[i] > ){
 
-
+  //   }
+  // }
+}
+// console.log(textBox);
