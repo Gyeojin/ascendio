8755bd496a92eb28c5a92907caaa1ea3c9a4fb32
Gyeojin <gohyeojin@gmail.com>
2021-10-18 01:31:43 +0900

    211018 / 가로 스크롤 이벤트 진행중

--- js/index.js
+++ js/index.js
@@ -7,16 +7,67 @@
 //   // }
 // });
 
-const textBox = document.querySelectorAll(".text_box p.desc");
-const textWidth = textBox.offsetWidth; //500px
-// const desc = textBox.lastChild.length;
-for (i = 0; i < textBox.length; i++) {
-  var desc = textBox[i].lastChild.length;
-  console.log(desc[i].offsetWidth);
-  // for (j = 0; j < desc.length; j++){
-  //   if(desc[i] > ){
+// const textBox = document.querySelectorAll(".text_box p.desc");
+// const textWidth = textBox.offsetWidth; //500px
+// // const desc = textBox.lastChild.length;
+// for (i = 0; i < textBox.length; i++) {
+//   var desc = textBox[i].lastChild.length;
+//   console.log(desc[i].offsetWidth);
+//   // for (j = 0; j < desc.length; j++){
+//   //   if(desc[i] > ){
 
-  //   }
-  // }
-}
-// console.log(textBox);
+//   //   }
+//   // }
+// }
+// // console.log(textBox);
+
+// $(function () {
+//   $(document).ready(function () {
+//     $(".artist_container").wheel(function (e, delta) {
+//       this.scrollLeft -= delta * 500;
+//       e.preventDefault();
+//     });
+//   });
+// });
+
+// $(function () {
+//   $(".artist_container").on("mousewheel", function (event, delta) {
+//     if (delta > 0) {
+//       $(this).scrollLeft -= delta * 500;
+//     } else if (delta < 0) {
+//       $(this).scrollLeft += delta * 500;
+//     }
+//   });
+// });
+
+$(function () {
+  // $(".artist").mouseover(function () {
+  //   //alert("MouseOver!"); 결과 : 성공
+  //   $(".artist_container").on("scroll touchmove mousewheel", (e) => {
+  //     $("body").scrollTop(0);
+  //   });
+  //   // $("body").on(
+  //   //   "scroll touchmove mousewheel",
+  //   //   function (e) {
+  //   //     e.preventDefault();
+  //   //     e.stopPropagation();
+  //   //     return false;
+  //   //   },
+  //   //   { passive: false }
+  //   // );
+  // });
+
+  $(".artist_container").on("mousewheel", function (e) {
+    $("body").scrollTop(0);
+    var wheelDelta = e.originalEvent.wheelDelta;
+    console.log(wheelDelta);
+    if (wheelDelta > 0) {
+      //스크롤 up 시 -150 찍힘
+      $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
+      //-(-150)+ 현재 요소의 스크롤 위치
+    } else {
+      //스크롤 down tl +150 찍힘
+      $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
+    }
+  });
+});
